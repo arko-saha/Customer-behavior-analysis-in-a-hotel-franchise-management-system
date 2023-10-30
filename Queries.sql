c772f8e7a78eb7b2d363435029fc77c60786ee4a
Arko Saha <64651886+arko-saha@users.noreply.github.com>
2023-10-30 04:43:34 +0000

    Create Queries.sql

---
+++ Queries.sql
@@ -0,0 +1,16 @@
+WITH hotels AS (
+SELECT * FROM dbo.[2018]
+UNION
+SELECT * FROM dbo.[2019]
+UNION
+SELECT * FROM dbo.[2020]
+)
+
+-- Check the night stays by year
+
+SELECT 
+arrival_date_year, 
+hotel,
+ROUND(SUM((CAST(stays_in_week_nights AS float) + CAST(stays_in_weekend_nights AS float)) * CAST(adr AS float)), 2) AS revenue 
+FROM hotels
+GROUP BY arrival_date_year, hotel;
