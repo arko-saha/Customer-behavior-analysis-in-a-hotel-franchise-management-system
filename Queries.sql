9d2d96a73d8318075d4350b4737d8b91e470bfee
Arko Saha <64651886+arko-saha@users.noreply.github.com>
2023-10-30 04:59:23 +0000

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -14,3 +14,14 @@ hotel,
 ROUND(SUM((CAST(stays_in_week_nights AS float) + CAST(stays_in_weekend_nights AS float)) * CAST(adr AS float)), 2) AS revenue 
 FROM hotels
 GROUP BY arrival_date_year, hotel;
+
+
+-- Combining the hotels table with the market_segment table by matching the market_segment column
+-- Then combining the hotels table with the meal_cost table by matching the meal column
+
+SELECT * FROM hotels
+LEFT JOIN dbo.market_segment
+ON hotels.market_segment = market_segment.market_segment
+LEFT JOIN dbo.meal_cost
+ON meal_cost.meal = hotels.meal;
+
